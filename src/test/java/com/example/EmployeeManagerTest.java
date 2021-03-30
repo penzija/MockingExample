33fa643ebf05a4d852d31dd332bea18c2e6e20d6
Luka Kik <luka.kik@iths.se>
2021-03-30 19:44:27 +0200

    Exercise 2 second test with Mockito

--- src/test/java/com/example/EmployeeManagerTest.java
+++ src/test/java/com/example/EmployeeManagerTest.java
@@ -1,21 +1,12 @@
 package com.example;
 
-import org.junit.jupiter.api.*;
-
-import static org.assertj.core.api.Assertions.assertThat;
-
-import static org.mockito.Mockito.*;
-
-import org.mockito.junit.jupiter.MockitoExtension;
-import org.w3c.dom.ls.LSOutput;
-
-import java.util.LinkedList;
-import java.util.List;
-
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 class EmployeeManagerTest {
 
@@ -31,4 +22,16 @@ class EmployeeManagerTest {
         assertEquals(2, actual);
     }
 
+    @Test
+    void checkIf_PayAllMethod_IsFillingCorrectly_withMockito() {
+        EmployeeRepository employeeRepository = mock(EmployeeRepository.class);
+        when(employeeRepository.findAll())
+                .thenReturn(List.of(new Employee("test", 1.0), new Employee("test2", 2.0)));
+        BankService bankService = mock(BankService.class);
+        EmployeeManager employeeManager = new EmployeeManager(employeeRepository, bankService);
+
+        var actual = employeeManager.payEmployees();
+
+        assertEquals(2, actual);
+    }
 }
