f45c7be023b848a56715c1ce85747c85d829a783
Luka Kik <luka.kik@iths.se>
2021-03-30 12:38:43 +0200

    Exercise 2 install Mockito

--- src/test/java/com/example/EmployeeManagerTest.java
+++ src/test/java/com/example/EmployeeManagerTest.java
@@ -2,6 +2,19 @@ package com.example;
 
 import org.junit.jupiter.api.*;
 
+import static org.mockito.Mockito.*;
+
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.w3c.dom.ls.LSOutput;
+
+import java.util.LinkedList;
+
 class EmployeeManagerTest {
 
+
+    public static void main(String[] args) {
+        LinkedList mockedList = mock(LinkedList.class);
+        when(mockedList.get(0)).thenReturn("This list does not really exist");
+        System.out.println(mockedList.get(0));
+    }
 }
